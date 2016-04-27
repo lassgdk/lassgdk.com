f0db7c424b12ecd64c0fb956a29be9185b831003
Decky Coss <coss@cosstropolis.com>
2016-04-27 15:45:34 -0400

    separate views file

--- lassgdk/__init__.py
+++ lassgdk/__init__.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
-@app.route("/")
-def index():
-    return "testing git pushing"
+from . import views
